fd1785bf49aa35901bf4dabfe477c360e2c0ecfc
donvno666 <donvno666@gmail.com>
2023-03-14 14:10:59 +0800

    Press enter to start now, and add some comments

--- typing-game/scripts.js
+++ typing-game/scripts.js
@@ -14,13 +14,19 @@ let startTime = Date.now();
 const quoteElement = document.getElementById('quote');
 const messageElement = document.getElementById('message');
 const typedValueElement = document.getElementById('typed-valued');
+const startButton = document.getElementById('start')
 
-document.getElementById('start').addEventListener('click', ()=>{
+// 在开始前禁止用户向文本框内输入内容
+typedValueElement.disabled = true;
+
+startButton.addEventListener('click', ()=>{
   const quotesIndex = Math.floor(Math.random()*quotes.length);
   const quote = quotes[quotesIndex];
+  // 拆分单词，存于数组中
   words = quote.split(' ');
   wordIndex = 0;
 
+  // map方法对类型化数组中的元素调用提供的 callback函数，按照顺序，并且会从结果构造新的类型化数组。
   const spanWords = words.map(function(word){ return `<span>${word} </span>`});
   quoteElement.innerHTML = spanWords.join('');
   quoteElement.childNodes[0].className = 'highlight';
@@ -30,7 +36,18 @@ document.getElementById('start').addEventListener('click', ()=>{
   typedValueElement.disabled = false;
   typedValueElement.focus();
 
+  // 开放输入
+  typedValueElement.disabled = false;
+  startButton.textContent = 'Try again';
+
   startTime = new Date().getTime();
+})
+
+// 按下enter相当于点击开始按钮
+window.addEventListener('keydown', function(event){
+  if (event.key === 'Enter') {
+    startButton.click();
+  }
 })
 
 typedValueElement.addEventListener('input', ()=>{
@@ -38,21 +55,27 @@ typedValueElement.addEventListener('input', ()=>{
   const typedValue = typedValueElement.value;
 
   if (typedValue === currentWord && wordIndex === words.length-1) {
+    // 检查是否全部完成
     const elapsedTime = new Date().getTime()-startTime;
     const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
     messageElement.innerText = message;
     typedValueElement.disabled = true;
   } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+    // 单个词汇检查
     typedValueElement.value = '';
     typedValueElement.className = '';
     wordIndex++;
+    // 重设所有引文子元素的className
     for (const wordElement of quoteElement.childNodes) {
       wordElement.className = '';
     }
+    // 标记新单词
     quoteElement.childNodes[wordIndex].className = 'highlight';
   } else if (currentWord.startsWith(typedValue)) {
+    // 单词输入正确
     typedValueElement.className = '';
   } else {
+    // 单词输入错误
     typedValueElement.className = 'error';
   }
 })
